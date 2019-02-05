fa9829ca36e803e53583efbae7119ba9e1ebab11
Tushar Verma <tushar.m.verma@gmail.com>
2019-02-05 19:40:07 -0400

    Useless docstring

--- Tools/Gui.py
+++ Tools/Gui.py
@@ -1,33 +1,3 @@
-#!/usr/bin/python
-
-# ---------------- READ ME ---------------------------------------------
-# This Script is Created Only For Practise And Educational Purpose Only
-# This is an Example Of Tkinter Canvas Graphics
-# This Script Is Created For http://bitforestinfo.blogspot.in
-# This Script is Written By
-#
-#
-##################################################
-######## Please Don't Remove Author Name #########
-############### Thanks ###########################
-##################################################
-#
-#
-__author__='''
-
-######################################################
-                By S.S.B Group                          
-######################################################
-
-    Suraj Singh
-    Admin
-    S.S.B Group
-    surajsinghbisht054@gmail.com
-    http://bitforestinfo.blogspot.in/
-
-    Note: We Feel Proud To Be Indian
-######################################################
-'''
 # Importing Modules
 try:
     import Tkinter, ttk
